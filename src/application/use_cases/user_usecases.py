6c43a8e4212dd2affd8aff8e8819ed4a6c538d02
NormanwOw <normjkeee@vk.com>
2025-03-29 14:43:09 +0500

    ADDED UpdateUser usecase

--- src/application/use_cases/user_usecases.py
+++ src/application/use_cases/user_usecases.py
@@ -1,7 +1,9 @@
 from src.application.use_cases.base import UseCase
 from src.domain.entities import User
+from src.domain.value_objects import UserData
 from src.infrastructure.models import UserModel
 from src.infrastructure.uow.interfaces import IUnitOfWork
+from src.presentation.routers.schemas import UserSchema
 
 
 class DeleteUser(UseCase[str, None]):
@@ -17,3 +19,16 @@ class DeleteUser(UseCase[str, None]):
                 domain_user: User = user.to_domain()
                 domain_user.delete_files()
                 await self.uow.commit()
+
+
+class UpdateUser(UseCase[UserData, UserSchema]):
+
+    def __init__(self, uow: IUnitOfWork):
+        self.uow = uow
+
+    async def __call__(self, user: User, new_user_data: UserData) -> UserSchema:
+        async with self.uow:
+            user = await self.uow.users.find_one(UserModel.id, user.id)
+            user.email = new_user_data.email
+            await self.uow.commit()
+            return UserSchema(email=user.email)

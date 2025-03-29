7f92f9da851810cbd2bfb6bbb898034588b555c1
NormanwOw <normjkeee@vk.com>
2025-03-29 14:57:20 +0500

    ADDED GetActiveUser and GetSuperuser usecases

--- src/application/use_cases/auth_usecases.py
+++ src/application/use_cases/auth_usecases.py
@@ -1,12 +1,16 @@
 from datetime import timedelta
 
+from fastapi.security import HTTPAuthorizationCredentials
+from fastapi.exceptions import HTTPException
+
 from config import Settings
 from src.application.use_cases.base import UseCase
-from src.domain.interfaces import IAuthService
-from src.infrastructure.external.base_client import Client
+from src.domain.entities import User
+from src.domain.services.auth_service import AuthService
+from src.infrastructure.external.yandex_client import YandexClient
 from src.infrastructure.models import UserModel
 from src.infrastructure.uow.interfaces import IUnitOfWork
-from src.presentation.routers.schemas import TokenData
+from src.presentation.routers.schemas import TokenData, UserSchema
 
 
 class YandexAuth(UseCase[str, TokenData]):
@@ -14,8 +18,8 @@ class YandexAuth(UseCase[str, TokenData]):
     def __init__(
             self,
             uow: IUnitOfWork,
-            yandex_client: Client,
-            auth_service: IAuthService,
+            yandex_client: YandexClient,
+            auth_service: AuthService,
             settings: Settings
     ):
         self.uow = uow
@@ -34,3 +38,47 @@ class YandexAuth(UseCase[str, TokenData]):
             )
             await self.uow.commit()
             return token
+
+
+class GetActiveUser(UseCase[HTTPAuthorizationCredentials, UserSchema]):
+
+    def __init__(
+            self,
+            uow: IUnitOfWork,
+            auth_service: AuthService,
+    ):
+        self.uow = uow
+        self.auth_service = auth_service
+
+    async def __call__(self, token_data: HTTPAuthorizationCredentials) -> User:
+        async with self.uow:
+            user_data = self.auth_service.get_user(token_data)
+            user: UserModel = await self.uow.users.find_one(UserModel.email, user_data.email)
+            if not user:
+                raise HTTPException(detail='User not found', status_code=404)
+            if not user.is_active:
+                raise HTTPException(detail='User is not active', status_code=400)
+
+            return user.to_domain()
+
+
+class GetSuperuser(UseCase[HTTPAuthorizationCredentials, UserSchema]):
+
+    def __init__(
+            self,
+            uow: IUnitOfWork,
+            auth_service: AuthService,
+    ):
+        self.uow = uow
+        self.auth_service = auth_service
+
+    async def __call__(self, token_data: HTTPAuthorizationCredentials) -> User:
+        async with self.uow:
+            user_data = self.auth_service.get_user(token_data)
+            user = await self.uow.users.find_one(UserModel.email, user_data.email)
+            if not user:
+                raise HTTPException(detail='User not found', status_code=404)
+            if not user.is_superuser:
+                raise HTTPException(detail='Permission denied', status_code=403)
+
+            return user.to_domain()

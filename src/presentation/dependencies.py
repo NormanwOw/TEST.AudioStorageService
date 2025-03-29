65513f8ecd3f420a6e5297ab60e08de8719ed5e7
NormanwOw <normjkeee@vk.com>
2025-03-29 17:40:17 +0500

    ADDED get_auth method to AuthDependencies

--- src/presentation/dependencies.py
+++ src/presentation/dependencies.py
@@ -3,12 +3,13 @@ from typing import Annotated
 from fastapi import Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-from config import settings
-from src.application.use_cases.auth_usecases import GetActiveUser, GetSuperuser, UpdateToken
+from config import Settings
+from src.application.use_cases.auth_usecases import GetActiveUser, GetSuperuser, UpdateToken, YandexAuth
 from src.application.use_cases.file_usecases import SaveFile
 from src.application.use_cases.user_usecases import DeleteUser, UpdateUser
 from src.domain.entities import User
 from src.domain.services.auth_service import AuthService
+from src.infrastructure.external.yandex_client import YandexClient
 from src.infrastructure.logger.logger import Logger
 from src.infrastructure.repositories.file_storage_repo import DiskStorageRepository
 from src.infrastructure.session import async_session
@@ -19,9 +20,15 @@ oauth_scheme = HTTPBearer()
 
 class AuthDependencies:
 
+    settings = Settings()
     uow = UnitOfWork(async_session)
     logger = Logger()
     auth_service = AuthService(settings)
+    yandex_client = YandexClient(settings)
+
+    @classmethod
+    async def get_auth(cls):
+        return YandexAuth(cls.uow, cls.yandex_client, cls.auth_service, cls.settings)
 
     @classmethod
     async def get_active_user(
@@ -39,7 +46,7 @@ class AuthDependencies:
 
     @classmethod
     async def update_token(cls):
-        return UpdateToken(cls.uow, cls.auth_service, settings)
+        return UpdateToken(cls.uow, cls.auth_service, cls.settings)
 
 
 class UserDependencies:
